4f461f2edf0becd19880d941a2b02087e350601b
GKuldeepak-Knoldus <76806497+GKuldeepak-Knoldus@users.noreply.github.com>
2021-03-17 18:26:55 +0530

    Delete Assessment-III directory

--- Assessment-III/build.sbt
+++
@@ -1,5 +0,0 @@
-name := "Assessment-III"
-
-version := "0.1"
-
-scalaVersion := "2.13.5"
--- Assessment-III/src/main/scala/List_Elements/List_impl.scala
+++
@@ -1,8 +0,0 @@
-package List_Elements
-
-object List_impl {
-  def main(args: Array[String]): Unit = {
-    val list_api = new List_api
-    print(list_api.getLastElement(List(5,7,2,4,5)))
-  }
-}
